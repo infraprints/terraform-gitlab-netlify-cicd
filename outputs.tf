758b12e4d2b023323ba571d687c1f1fe6d96a5a3
jrbeverly <jrbeverly@live.com>
2019-03-22 22:08:22 -0400

    Adjust the documentation

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@ output "id" {
   value       = "${netlify_site.main.id}"
 }
 
-output "custom_domain" {
-  description = "a custom domain name, must be configured using a cname in accordance with [netlify's docs](https://www.netlify.com/docs/custom-domains/)"
-  value       = "${netlify_site.main.custom_domain}"
+output "name" {
+  description = "Name of your site on netlify."
+  value       = "${netlify_site.main.name}"
 }
 
-output "name" {
-  description = "name of your site on netlify"
-  value       = "${netlify_site.main.name}"
+output "custom_domain" {
+  description = "A custom domain name, must be configured using a cname in accordance with [netlify's docs](https://www.netlify.com/docs/custom-domains/)."
+  value       = "${netlify_site.main.custom_domain}"
 }
